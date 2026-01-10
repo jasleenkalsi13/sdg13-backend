b100d00ae066481e7e5669db77ec3cf3e14f2fcf
jasleenkalsi13 <kalsijasleenkaur@gmail.com>
2026-01-10 20:08:29 +0530

    Fix Flask app initialization and main check

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
@@ -33,6 +33,6 @@ def carbon():
         "impact_level": level
     })
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run()
 
